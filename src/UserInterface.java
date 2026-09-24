abab7d2b3088849e6d0b55c25360a57960be57f5
Nour El-Sayed <noel1000@stud.ek.dk>
2026-09-24 19:58:57 +0200

    Adventure bruger nu Map og Player

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,19 +1,26 @@
-public class Userinterface {
+public class UserInterface { // Userinterface står for al kommunikation med brugeren.
 
-    private Adventure adventure; //kalder konstruktøren i vores Adventure-klasse.
+    // Userinterface kender kun Adventure (controlleren) - ikke Player, Map eller Room (lav kobling)
+    private Adventure adventure;
 
-    public Userinterface() {
+    // Konstruktør: opretter spillet.
+    public UserInterface() {
         adventure = new Adventure();
     }
 
+
+    // Starter spillet og kører, indtil brugeren skriver "exit".
     public void start() {
 
         boolean playing = true;
 
+        // Velkomsttekst
         IO.println("Welcome to Adventure time");
         IO.println("Can you find your way around the caves of Ooo, maybe there is a hidden treasure?");
         IO.println("You can traverse around the cave and use different commands, for example 'help'");
 
+
+        // Spil-løkken: læser en kommando, udfører den og starter forfra.
         while (playing) {
             String kommando = IO.readln("> ");
 
@@ -23,10 +30,7 @@ public class Userinterface {
                     playing = false;
                 }
                 case "look" -> {
-                    Room currentRoom = adventure.getCurrentRoom();//kalder look metoden fra vores adventure klasse
-
-                IO.print(currentRoom.getName());
-                IO.print(currentRoom.getDescription());
+                    IO.println(adventure.look());//kalder look metoden fra vores adventure klasse
                 }
 
                 case "help" -> {
@@ -36,6 +40,8 @@ public class Userinterface {
                     IO.println("exit = quit the game ");
                 }
 
+                // Her oversættes både "n" og "north" til goNorth()
+                // Metoderne returnerer true, hvis spilleren flyttede sig, ellers false.
                 case "n", "north" -> {
                     if (adventure.goNorth()) {
                         IO.println("You moved north");
@@ -50,13 +56,7 @@ public class Userinterface {
                         IO.println("You cannot go that way");
                     }
                 }
-                case "w", "west" -> {
-                    if (adventure.goWest()) {
-                        IO.println("You moved west");
-                    } else {
-                        IO.println("You cannot go that way");
-                    }
-                }
+
                 case "s", "south" -> {
                     if (adventure.goSouth()) {
                         IO.println("You moved south");
@@ -64,7 +64,18 @@ public class Userinterface {
                         IO.println("You cannot go that way");
                     }
                 }
+
+                case "w", "west" -> {
+                    if (adventure.goWest()) {
+                        IO.println("You moved west");
+                    } else {
+                        IO.println("You cannot go that way");
+                    }
+                }
+
             }
         }
     }
 }
+
+
